262c8a6f5aa6c62950334941d1d17e28f4fe8207
erinseo222 <erinseo@192-168-1-103.tpgi.com.au>
2021-10-21 23:09:31 +1100

    first commit

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,8 @@
 # This program prints Hello, world!
 
+print('--------------------------')
+print('|  01HelloWorld          |')
+print('|  Name: Erin Seo        |')
+print('|  Version: 01           |')
+print('--------------------------')
 print('Hello, world!')
