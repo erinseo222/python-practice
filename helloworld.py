947948f1644ae6bb5c4dc8da89c5f1410b8e3a7e
erinseo222 <erinseo@192-168-1-103.tpgi.com.au>
2021-10-26 22:52:10 +1100

    extend the border

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,8 @@
 # This program prints Hello, world!
 
-print('--------------------------')
-print('|  01HelloWorld          |')
-print('|  Name: Erin Seo        |')
-print('|  Version: 01           |')
-print('--------------------------')
-print('Hello, world!')
+print('------------------------------------')
+print('|   01HelloWorld                    |')
+print('|   Name: Erin Seo                  |')
+print('|   Version: 01                     |')
+print('------------------------------------')
+print('Hello, world')
