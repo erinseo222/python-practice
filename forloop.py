4c6dd3411e041ddcda599c81b11af3350baea0bc
erinseo222 <erinseo@Erins-MacBook-Air.local>
2021-10-28 10:44:34 +1100

    for loop

---
+++ forloop.py
@@ -0,0 +1,10 @@
+# This program prints Hello, world! 
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    05ForLoop                                   |")
+print("|    Name : Erin Seo                             |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+for x in range(1000) :
+    print(x)
