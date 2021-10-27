0163ab515b7e8562cba94704652adc3c60c54668
erinseo222 <erinseo@Erins-MacBook-Air.local>
2021-10-28 10:39:19 +1100

    goodbye teacher 1

---
+++ goodbyeteacher.py
@@ -0,0 +1,13 @@
+# This program prints Hello, world! 
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    04GoodbyeTeacher                            |")
+print("|    Name : Erin Seo                             |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+name=input("Mr Horan")
+if name == "Mr Horan"
+    print("Mr Horan is a great teacher")
+else:
+    print("Mr Horan is not a good teacher")
