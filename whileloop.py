180498a6b4a8a0f64e6e8683da2ec46b54918999
erinseo222 <erinseo@Erins-MacBook-Air.local>
2021-11-04 09:08:33 +1100

    whileloop

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+# This program prints Hello, world! 
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    06WhileLoop                                 |")
+print("|    Name : Erin Seo                             |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+x=input("What is the name of this subject ")
+while x!="IST":
+      print("Not Correct - try again")
+      x=input("What is the name of this subject ")
+else:
+      print("")
+      print("")
+      print("Congratulations!!")
+      print("")
+      print("")
+      print("")
